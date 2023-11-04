8be61a6f815e7f62bf3998675e6cb7d9005c239e
sushant <sushantmech384@gmail.com>
2023-11-05 01:56:19 +0530

    firstcommit

---
+++ src/assets/data/portfolioData.js
@@ -0,0 +1,108 @@
+import portfolioImg01 from "../images/portfolio-01.jpg";
+import portfolioImg02 from "../images/portfolio-02.jpg";
+import portfolioImg03 from "../images/portfolio-03.jpg";
+import portfolioImg04 from "../images/portfolio-04.jpg";
+import portfolioImg05 from "../images/portfolio-04.png";
+import portfolioImg06 from "../images/portfolio-06.jpg";
+import portfolioImg07 from "../images/portfolio-07.jpg";
+
+const portfolios = [
+  {
+    id: "01",
+    imgUrl: portfolioImg01,
+    category: "Web Design",
+    title: "eCommerce Web App",
+    description:
+      "This is a website for online shopping that can be used to react liberally. This is a list of several products that we may add, remove, and add to our cart. The price of each product is added to our cart and is automatically transferred to the section that contains the prices of all of the products. The Products Can Also Be Searched.",
+    technologies: [
+      "React",
+      "Tailwind CSS",
+      "Javascript",
+      "React-redux-toolkit",
+    ],
+    siteUrl: "https://sushant-e-commerce-website.netlify.app",
+  },
+  {
+    id: "02",
+    imgUrl: portfolioImg02,
+    category: "Web Design",
+    title: "Hairstyle Salon",
+    description:
+      "I made a beautiful hairstyle salon website using a custom Toggle Bar menu with HTML, CSS, and Java Script. This website also features a lovely animation.",
+    technologies: ["Javascript", "HTML", "CSS"],
+    siteUrl: "https://beautifull-animation-sallon.netlify.app/",
+  },
+  {
+    id: "03",
+    imgUrl: portfolioImg03,
+    category: "Web Design",
+    title: "Restaurant Website",
+    description:
+      "I made a beautiful Restaurant website using a custom Toggle Bar menu with HTML, CSS, and Java Script. This website also features a lovely animation.",
+    technologies: ["Javascript", "HTML", "CSS"],
+    siteUrl: "https://github.com/sushantkr384/restaurant-website-sushant.git",
+  },
+  {
+    id: "04",
+    imgUrl: portfolioImg05,
+    category: "Web Design",
+    title: "My Portfolio",
+    description:
+      "This is my portfolio website that can be used to react liberally. Different types of web pages are present in this website, We can see all the details of the different types of projects. This website also features a lovely animation.",
+    technologies: ["React", "Tailwind CSS", "Javascript"],
+    siteUrl: "#",
+  },
+  {
+    id: "05",
+    imgUrl: portfolioImg03,
+    category: "Web Design",
+    title: "Landing Page",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+
+  {
+    id: "06",
+    imgUrl: portfolioImg06,
+    category: "Ux",
+    title: "Online Therapy Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "07",
+    imgUrl: portfolioImg07,
+    category: "Web Design",
+    title: "Appointment Booking Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "08",
+    imgUrl: portfolioImg01,
+    category: "Web Design",
+    title: "Finance Technology Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+  {
+    id: "09",
+    imgUrl: portfolioImg02,
+    category: "Ux",
+    title: "Video Conference Website",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium vitae sunt magnam numquam esse ipsam? Quis qui enim, temporibus dignissimos atque repellendus amet a velit. Dolorem quidem animi doloribus saepe!",
+    technologies: ["React", "Tailwind css", "Node.js", "MongoDB"],
+    siteUrl: "#",
+  },
+];
+
+export default portfolios;
---
+++ src/components/UI/Hero.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import sushant from "../../assets/images/sushant-removebg-preview.png";
+// import CountUp from "react-CountUp";
+const Hero = () => {
+  return (
+    <section className="pt-0" id="about">
+      <div className="container pt-14">
+        <div className="md:flex items-center justify-between sm:flex-col md:flex-row">
+          {/* =========hero left container============== */}
+          <div className="w-full md:basis-1/2">
+            <h5
+              data-aos="fade-right"
+              data-aos-duration="1500"
+              className="text-stone-600 font-[600] text-[16px]"
+            >
+              welcome
+            </h5>
+            <h1
+              data-aos="fade-up"
+              data-aos-duration="1500"
+              className="text-stone-600 font-[800] text-[18px] sm:text-[30px] leading-[35px] sm:leading-[46px] mt-3"
+            >
+              Hey, I am Sushant Kumar <br />
+              Web Developer
+            </h1>
+            <div
+              data-aos-duration="1800"
+              data-aos-delay="200"
+              className="flex items-center gap-6 mt-7"
+            >
+              <a href="#contact">
+                <button
+                  className="bg-blue-500 text-white font-[500] flex items-center gap-2
+                   hover:bg-blue-800 ease-in duration-300 py-2 px-4 rounded-md"
+                >
+                  <i className="ri-mail-line"></i> Hire Me
+                </button>
+              </a>
+              <a
+                href="#portfolio"
+                className="bg-blue-500 font-[500] text-[16px] border-b border-solid border-blue-500 py-2 px-4 rounded-md"
+              >
+                See Portfolio
+              </a>
+            </div>
+            <p
+              data-aos-duration="1500"
+              data-aos="fade-left"
+              className="flex gap-2 text-blue-500 mt-14 font-[500] text-[18px] leading-7 sm:pl-14 sm:pr-10"
+            >
+              <span>
+                <i className="ri-apps-2-line"></i>
+              </span>
+              My name is Sushant Kumar, and I have developed front-end websites
+              for the past two years.I am capable of creating a user-friendly,
+              stunning, and responsive website.
+            </p>
+            <div className="flex items-center gap-9 mt-14">
+              <span className="text-blue-600 text-[15px] font-[600]">
+                Follow Me
+              </span>
+              <span className="flex gap-2">
+                <a
+                  href="https://twitter.com/Sushant34010369"
+                  className="text-blue-500 text-[21px] font-[600]"
+                >
+                  <i className="ri-twitter-x-line"></i>
+                </a>
+                <a href="https://www.facebook.com/profile.php?id=100008750569389" className="text-blue-500 text-[21px] font-[600]">
+                  <i className="ri-facebook-box-fill"></i>
+                </a>
+                <a href="https://github.com/sushantkr384" className="text-blue-500 text-[21px] font-[600]">
+                  <i className="ri-github-fill"></i>
+                </a>
+              </span>
+            </div>
+          </div>
+
+          {/* =========hero left container end============== */}
+
+          {/* =========hero img============== */}
+          <div className="basis-1/3 mt-10 sm:mt-0">
+            <figure className="flex items-center justify-center">
+              <img src={sushant} alt="" />
+            </figure>
+          </div>
+          {/* =========hero img end============== */}
+
+          {/* =========hero content right============== */}
+
+          <div
+            className="md:basis-1/5 flex justify-between text-center mt-10 flex-wrap gap-3
+           md:mt-0 md:flex-col md:justify-end md:text-end"
+          >
+            <div className="mb-10">
+              {/* <h2 className="text-blue-500 font-[700] text-[32px]">
+                <CountUp start={0} end={2} suffix="+" />
+              </h2> */}
+              <h4 className="text-blue-500 font-[600] text-[18px]">
+                2 years of experience
+              </h4>
+            </div>
+          </div>
+
+          {/* =========hero content right end============== */}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
