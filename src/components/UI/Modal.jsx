f95f096cba8d314439a899826fd8f980b0280e56
sushant <sushantmech384@gmail.com>
2023-11-08 01:28:57 +0530

    new commit

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -4,24 +4,24 @@ import Portfolios from "../../assets/data/portfolioData";
 const Modal = ({ activeID, setShoeModal }) => {
   const portfolio = Portfolios.find((portfolio) => portfolio.id === activeID);
   return (
-    <div className="w-full h-full fixed top-0 left-0 z-10 bg-black bg-opacity-40">
+    <div className="w-full h-full fixed top-0 left-0 z-10 bg-black bg-opacity-40 ">
       <div className="md:max-w-[600px]  absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform-translate-x-1/2 -translate-y-1/2 p-5">
-        <div>
+        <div className="pt-74px">
           <figure>
-            <img src={portfolio.imgUrl} alt="" />
+            <img src={portfolio.imgUrl} alt="my-image" />
           </figure>
         </div>
 
         <div>
-          <h2 className="text-2xl text-black font-[700] my-5">
+          <h2 className="text-2xl text-black font-[700] my-3">
             {portfolio.title}
           </h2>
 
-          <p className="text-[15px] leading-7 text-green-600">
+          <p className="text-[15px] leading-4 text-green-900">
             {portfolio.description}
           </p>
 
-          <div className="flex mt-[5] items-center gap-3 flex-wrap">
+          <div className="flex mt-[6] items-center gap-2 flex-wrap">
             <h4 className="text-[10px] text-black text-[800]">
               technologies :
             </h4>
@@ -29,7 +29,7 @@ const Modal = ({ activeID, setShoeModal }) => {
             {portfolio.technologies.map((item, index) => (
               <span
                 key={index}
-                className="bg-black py-1 px-2 
+                className="bg-black py-0 px-2 
               rounded-[5px] text-[14px] leading-1"
               >
                 {item}
