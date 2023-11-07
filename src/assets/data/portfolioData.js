90424bbb3aeaad07d58245ffbe30bb167240d98e
sushant <sushantmech384@gmail.com>
2023-11-07 21:29:10 +0530

    new commit

--- src/assets/data/portfolioData.js
+++ src/assets/data/portfolioData.js
@@ -1,7 +1,6 @@
 import portfolioImg01 from "../images/portfolio-01.jpg";
 import portfolioImg02 from "../images/portfolio-02.jpg";
 import portfolioImg03 from "../images/portfolio-03.jpg";
-import portfolioImg04 from "../images/portfolio-04.jpg";
 import portfolioImg05 from "../images/portfolio-04.png";
 import portfolioImg06 from "../images/portfolio-06.jpg";
 import portfolioImg07 from "../images/portfolio-07.jpg";
